be952f381e3e8e5d21ac14e5a48b36b930694cac
Andrey Gurenko <89606947+AN-LITE@users.noreply.github.com>
2022-03-15 17:24:42 +0300

    HW_3_Gurenko

---
+++ HW_3_js/js/script.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// Task #1
+
+let admin = 'John';
+let name = 'John';
+alert (admin);
+
+// Task #2
+
+let planetName = 'Kepler-452';
+let userName = prompt ('Как тебя звать,дружище?');
+alert (`Здоров, ${userName}, шо ты?`);
+
+// Task #3
+
+let calc = '10' + 10;
+console.log(calc);
+
+// Потому что разные типы данных не могут суммироваться,приобразовывая результат сложения.  Из-за этого они просто клеяться друг к другу.
