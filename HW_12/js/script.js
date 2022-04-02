230838885efb64cb833b8c727b2d1a5213e9cf00
Andrey Gurenko <89606947+AN-LITE@users.noreply.github.com>
2022-04-02 22:17:55 +0300

    HW_12

---
+++ HW_12/js/script.js
@@ -0,0 +1,96 @@
+'use strict'
+
+// 1.
+
+
+// let x = 10, y = 7;
+
+// (x > y)? ('x больше чем y') : ('x не больше, чем y');
+
+
+// 2.
+
+
+// let num = prompt('введите число');
+
+
+// if(num % 2 == 0){
+//     alert ('число ' + num + ' четное');
+
+// }else {
+//     alert('число ' + num + ' нечетное');
+// }
+
+
+// 3.
+
+
+
+// let ask = prompt ('введите число');
+// console.log(a.length);
+
+// if (ask > 0){
+
+//     alert(`число положительное \n ${ask.length}`)
+
+// }else {
+//     alert(`число отрицательное \n ${ask.length}`)
+// }
+    
+
+
+// 4.
+
+// let first = +prompt ('введите первое число')
+// let second = +prompt ('введите второе число')
+// let third = +prompt ('введите третье число')
+
+// if (first > second && first > third ){
+
+//     alert (first)
+
+// }else if (second > third && second > first){
+
+//     alert (second)
+
+// }else if (third > first && third > second){
+
+//     alert (third)
+
+// }else {
+//     alert ('числа равны');
+// }
+
+
+// 5.
+
+// let a = prompt('Длина одной стороны')
+// let b = prompt('Длина второй стороны')
+// let c = prompt('Длина третьей стороны')
+
+// if ((a + b) > c){
+//     alert ('треугольник может быть построен')
+
+
+// }else if ((a + c) > b){
+
+//     alert ('треугольник может быть построен')
+
+// }else if ((b + c) > a){
+
+//     alert ('треугольник может быть построен')
+
+// }else {
+//     alert('треугольник не может быть построен')
+// }
+
+
+
+
+
+
+
+
+
+
+
