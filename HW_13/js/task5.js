69411cade4e3deaecbf43d64cb9f0f520a4c12b7
Andrey Gurenko <89606947+AN-LITE@users.noreply.github.com>
2022-04-04 19:38:11 +0300

    HW_13

--- HW_13/js/task5.js
+++ HW_13/js/task5.js
@@ -1,11 +1,13 @@
-// 'use strict'
+'use strict'
 
-// const arr = [1, 2, 3, 4, 5];
+const arr = [1, 2, 3, 4, 5];
 
-// let sum = 0;
+let sum = 0;
 
-// for (let i = 0; i < arr.length; i++){
+for (let i = 0; i < arr.length; i++){
 
-//     sum += Math.pow(arr.length,2)
-// }
-// console.log(sum)
+    // sum += Math.pow(arr[i],2)
+    sum += arr[i]**2
+    
+}
+console.log(sum)
