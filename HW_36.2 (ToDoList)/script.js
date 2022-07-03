bc8a19d6fa09370661b2916b9c7cacc0effc3a4d
Andrey Gurenko <89606947+AN-LITE@users.noreply.github.com>
2022-07-03 12:13:52 +0300

    HW_36

--- HW_36.2 (ToDoList)/script.js
+++ HW_36.2 (ToDoList)/script.js
@@ -3,16 +3,14 @@
 const addBtn = document.getElementById('btn_list');
 const setInput = document.getElementById('description_task');
 const toDoWrap = document.querySelector('.todos_wrapper');
-
 let tasks = null;
 let toDoItems = [];
 
 !localStorage.tasks ? tasks = [] : tasks =  JSON.parse(localStorage.getItem('tasks'));
 
-function task (description){
-    this.description = description
-    this.completed = false
-    this.pending = false
+function task (description){ 
+    this.description = description;
+    this.status = 'no-status';
 }
 
 const createTemplate = (task, index) =>{
@@ -21,13 +19,13 @@ const createTemplate = (task, index) =>{
         <div class="description">${task.description}</div>
             <div class="wrapper_buttons">
 
-                <select class="todos_status" >
-                    <option onclick ='No-status(${index}) 'value="no-status">No-status</option> 
-                    <option onclick ='completeTask(${index})' value="completed"  ${task.completed ? 'completed' : ""}>Completed</option>
-                    <option onclick ='pendingTask(${index})' value="pending"  ${task.pending ? 'pending' : ""}>Pending</option>   
+                <select class="todos_status" data-index="(${index})">
+                    <option value="no-status">No-status</option> 
+                    <option value="completed">Completed</option>
+                    <option value="pending">Pending</option>   
                 </select>
                 
-                <button onclick="deleteTask(${index})" class="btn_delete">Delete</button>
+                <button class="btn_delete" data-index="(${index})">Delete</button>
             </div>   
     </div> 
     `
@@ -48,46 +46,26 @@ const fillTodo = () => {
 
 fillTodo();
 
-const localUp = () =>{
-    localStorage.setItem('tasks', JSON.stringify(tasks))
+const localUp = (data) =>{
+    localStorage.setItem('tasks', JSON.stringify(data || tasks))
 }
 
-// setSelect.addEventListener('change', () =>{
-//     this.value
-
-// });
-
-const completeTask = index => {
-
-    tasks[index].copmleted = !tasks[index].copmleted;
-    console.log(index);
-
-    // if(tasks[index].copmleted){
-    //     toDoItems[index].classList.add('complete');
-    // }else{
-    //     toDoItems[index].classList.remove('complete');
-    // }
-    localUp();
-    fillTodo();   
-}
 
 addBtn.addEventListener('click', () => {
 
-    if(setInput.value == ''){
-        return
+    if(setInput.value.trim() === '') return
 
-    }else{       
-        tasks.push(new task(setInput.value))
-        localUp();
-        fillTodo(); 
-        setInput.value = ''  
-    }
+    tasks.push(new task(setInput.value))
+    localUp();
+    fillTodo(); 
+    setInput.value = ''  
+    
 
 })
 
 const deleteTask = index =>{
 
-    toDoItems[index].classList.add('deletion');
+    // toDoItems[index].classList.add('deletion');    
         
     setTimeout(()=>{ 
         tasks.splice(index,1)
@@ -97,4 +75,36 @@ const deleteTask = index =>{
 
 }
 
+toDoWrap.addEventListener('click', event =>{
+
+    event.stopPropagation();
+
+    if(!event.target.classList.contains('btn_delete')) return;
+
+    const index = event.target.getAttribute('data-index');
+    deleteTask(index);
+});
+
+
+toDoWrap.addEventListener('change', event => {
+
+    event.stopPropagation();
+
+    if(!event.target.classList.contains('todos_status')) return;
+   
+    const index = event.target.getAttribute('data-index')
+
+    const data = structuredClone(tasks);
+
+    if(!data.length) return;
+    console.log(event.target.value);
+
+    // data[index].status = event.target.value
+
+    
+    
+    localUp(data);
+
+    
+});
 
